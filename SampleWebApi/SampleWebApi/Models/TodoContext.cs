a7d84ec9164ee49370ea856b62a53ae99b8e23cd
Manoj Choudhari <manoj_choudhari@outlook.com>
2023-09-26 22:58:36 +0200

    SampleWebApi

---
+++ SampleWebApi/SampleWebApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+
+namespace SampleWebApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> TodoItems { get; set; } = null!;
+    }
+}
